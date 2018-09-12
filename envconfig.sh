8b74861439095d0cae30943ac3c9b8d1d676d480
Brent Langston <brentley@oufan.com>
2018-09-12 21:45:34 +0200

    taking out env steps

--- envconfig.sh
+++ envconfig.sh
@@ -2,12 +2,6 @@
 
 set -vx
 
-echo Env:
-printenv
-
-echo 
-echo
-
 # read env variables and write the clair config file
 cd /
 cat /config.yaml.sample | sed -e "s/host=localhost/host=${PGDATABASE}/" -e "s/user=postgres/user=${PGUSER}/" > /etc/clair/config.yaml
